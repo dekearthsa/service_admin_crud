a49b47b1ad13712299ce93b5de5ecc963538ab2e
pcsishun <pcsishun@gmail.com>
2023-10-05 14:42:04 +0700

    remove key

--- main.go
+++ main.go
@@ -16,32 +16,6 @@ func main() {
 	router.POST("/api/upload", controller.ControllerUploadFile)
 	router.POST("/api/update/product", controller.ControllerProduct)
 	router.POST("/api/update/content", controller.ControllerContent)
-	// router.POST("/api/test/user", func(c *gin.Context) {
-
-	// 	var res model.Product
-	// 	ctx := context.Background()
-
-	// 	// log.Println("1. res => ", res)
-	// 	if err := c.BindJSON(&res); err != nil {
-	// 		log.Fatal("err BindJSON =>", err)
-	// 	}
-
-	// 	client, err := datastore.NewClient(ctx, "scg-iat-project-coretech")
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 		c.JSON(200, gin.H{"message": "can't find projectID."})
-	// 	}
-
-	// 	// log.Println("2. res => ", res)
-
-	// 	// log.Println("3. e1 => ", e1)
-	// 	key := datastore.IncompleteKey("go_product", nil)
-	// 	if _, err := client.Put(ctx, key, &res); err != nil {
-	// 		log.Fatal(err)
-	// 		c.JSON(200, gin.H{"message": "can't insert data."})
-	// 	}
-	// 	c.JSON(200, gin.H{"status": "Inserted."})
-	// })
 
 	err := router.Run(port)
 	if err != nil {
