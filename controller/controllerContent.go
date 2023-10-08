2e616d139a67a09d8587802478481d6356547ada
pcsishun <pcsishun@gmail.com>
2023-10-08 16:47:26 +0700

    update: upload muti task

--- controller/controllerContent.go
+++ controller/controllerContent.go
@@ -1,13 +1,33 @@
 package controller
 
 import (
+	"context"
+	"log"
 	"net/http"
 	"service_admin_crud/model"
 
+	"cloud.google.com/go/datastore"
 	"github.com/gin-gonic/gin"
 )
 
-func ControllerContent(ctx *gin.Context) {
-	var setContent model.Content
-	ctx.JSON(http.StatusOK, gin.H{"status": setContent})
+func ControllerContent(c *gin.Context) {
+	var req model.Content
+	if err := c.BindJSON(&req); err != nil {
+		log.Println("err BindJSON =>", err)
+		// c.JSON(http.StatusBadRequest, gin.H{"message": "payload is empty"})
+	}
+	ctx := context.Background()
+	clientDatastore, err := datastore.NewClient(ctx, "scg-iat-project-coretech")
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "can't find projectID."})
+	}
+
+	key := datastore.IncompleteKey("go_product", nil)
+	if _, err := clientDatastore.Put(ctx, key, &req); err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "can't insert data."})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
 }
--- controller/controllerUploadImg.go
+++ controller/controllerUploadImg.go
@@ -14,11 +14,11 @@ import (
 
 func ControllerUploadImage(c *gin.Context) {
 	uid := c.Request.Header.Get("userID")
-	setFileName, err := c.FormFile("images")
-	if err != nil {
-		log.Println("err c.FormFile(images)", err)
-		c.JSON(http.StatusNoContent, gin.H{"Status": "not found file name."})
-	}
+	// setFileName, err := c.FormFile("images")
+	// if err != nil {
+	// 	log.Println("err c.FormFile(images)", err)
+	// 	c.JSON(http.StatusNoContent, gin.H{"Status": "not found file name."})
+	// }
 
 	form, err := c.MultipartForm()
 	ctx := context.Background()
@@ -33,40 +33,40 @@ func ControllerUploadImage(c *gin.Context) {
 		// c.JSON(http.StatusNoContent, gin.H{"Status": "file is empty."})
 	}
 
-	size := files[0].Size
-	if size >= 5000000 {
-		log.Println("err before files[0].Size => ", err)
-		c.JSON(http.StatusRequestHeaderFieldsTooLarge, gin.H{"Status": "file must less than 5MB."})
-	}
+	for _, file := range files {
+		size := file.Size
+		if size >= 5000000 {
+			log.Fatal("err before files[0].Size => ", err)
+			c.JSON(http.StatusRequestHeaderFieldsTooLarge, gin.H{"Status": "file must less than 5MB."})
+		}
 
-	src, err := files[0].Open()
-	if err != nil {
-		log.Println("err files[0].Open() => ", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"Status": "file"})
-	}
-	defer src.Close()
+		src, err := file.Open()
+		if err != nil {
+			log.Println("err files[0].Open() => ", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"Status": "file"})
+		}
+		defer src.Close()
 
-	imagePath := uid + "_" + setFileName.Filename
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		log.Println("err storage.NewClient(ctx) => ", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"Status": "internal error."})
-	}
+			 := uid + "_" + file.Filename
+		client, err := storage.NewClient(ctx)
+		if err != nil {
+			log.Println("err storage.NewClient(ctx) => ", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"Status": "internal error."})
+		}
 
-	bucket := client.Bucket("demostoragebucketearth")
-	wc := bucket.Object(imagePath).NewWriter(ctx)
-	_, err = io.Copy(wc, src)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusServiceUnavailable, gin.H{"status": "StatusServiceUnavailable"})
+		bucket := client.Bucket("demostoragebucketearth")
+		wc := bucket.Object(imagePath).NewWriter(ctx)
+		_, err = io.Copy(wc, src)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusServiceUnavailable, gin.H{"status": "StatusServiceUnavailable"})
+		}
+		err = wc.Close()
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusServiceUnavailable, gin.H{"status": "StatusServiceUnavailable"})
+		}
 	}
-
-	err = wc.Close()
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusServiceUnavailable, gin.H{"status": "StatusServiceUnavailable"})
-	}
-
 	c.JSON(http.StatusOK, gin.H{"Status": "Upload image sucess."})
 
 }
